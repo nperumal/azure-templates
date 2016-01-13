e70c23300c07570e13ecea84be62820a98f71d5e
nperumal <nperumal@pros.com>
2016-01-13 11:46:08 -0800

    Initial version of storage account template and script

---
+++ shared-resources/hdfs/storage/storage-account-container.ps1
@@ -0,0 +1,20 @@
+﻿$resourceGroupName = 'SharedResourcesDev'
+$subscriptionname = Read-Host 'Enter Subscription Name' 
+$clientId=Read-Host 'Enter storage container(Tenant Id)'
+$storageAccountName=Read-Host 'Enter storage account'
+$storageContainer = $clientId.ToLower()
+$storageAccountName = $StorageAccountName.ToLower()
+$location = "West US"
+
+$param = "{""newStorageAccountName"":{""value"": $($StorageAccountName)}, ""storageAccountType"":{""value"": ""Standard_LRS""}, ""location"":{""value"": $($location)}, ""container"":{""value"": $($clientId)}}"
+
+Select-AzureRmSubscription -SubscriptionName $subscriptionname
+New-AzureRmResourceGroup -Name $resourceGroupName  -Location $location -ErrorAction Continue -Force
+New-AzureRmResourceGroupDeployment -Name SAGoldDeployment -ResourceGroupName $resourceGroupName -Mode Incremental -TemplateFile C:\prosarmtemplates\create-storage-account-standard\azuredeploy.json -newStorageAccountName $storageAccountName -storageAccountType "Standard_LRS" -location $location -container $clientId
+#-TemplateParameterFile C:\prosarmtemplates\create-storage-account-standard\azuredeploy.parameters.json
+$storageKey = Get-AzureRmStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName
+$storageContext = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageKey.Key1
+if (!(Get-AzureStorageContainer -Context $storageContext -Name $storageContainer -ErrorAction SilentlyContinue) )  {
+    New-AzureStorageContainer -Context $StorageContext -Name $storageContainer -Permission Off
+}
+Write-Host "wasb://$($storageContainer)@$($storageAccountName).blob.core.windows.net/"
